e932b05506377686f1dc1f66685b19e12f049f94
Siamac Nikoo <nikoos@landcareresearch.co.nz>
2020-05-15 15:14:04 +1200

    Init

---
+++ Models/ContactUsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystematicPortal.Models
+{
+    public class ContactUsViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Organisation { get; set; }
+
+        public string Location { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        public ContactUsViewModel()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            Organisation = string.Empty;
+            Location = string.Empty;
+            Subject = string.Empty;
+            Message = string.Empty;
+        }
+    }
+}
---
+++ Models/JSONBaseModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystematicsPortal.Models
+{
+    public class JSONBaseModel
+    {
+        public string Information { get; set; }
+        public string Error { get; set; }
+    }
+
+    public class JSONRequestConfirmationModel : JSONBaseModel
+    {
+        public string Request { get; set; }
+    }
+
+    public class JSONSpecimenAddedToSetModel : JSONBaseModel
+    {
+        public List<SetSummary> Sets { get; set; }
+    }
+
+    public class SetSummary
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class JSONUpdatedProfileModel : JSONBaseModel
+    {
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public bool EmailUpdated { get; set; }
+    }
+
+    public class JSONToggleSpecimenSelectionModel : JSONBaseModel
+    {
+        public bool OneOrMoreSelected { get; set; }
+    }
+}
