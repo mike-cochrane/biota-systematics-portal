f00c62cade1a6fbf3be357e7b3a69c0606d8e1ab
Siamac Nikoo <nikoos@landcareresearch.co.nz>
2020-05-26 14:33:40 +1200

    Copied over new MWLR website design as base for modifications.

--- Systematics.Portal.Web/Controllers/SupportController.cs
+++ Systematics.Portal.Web/Controllers/SupportController.cs
@@ -46,7 +46,7 @@ namespace Systematics.Portal.Web.Controllers
         {
             return View();
         }
-        public IActionResult ReleaseNotes()
+        public IActionResult LatestChanges()
         {
             return View();
         }
@@ -58,71 +58,6 @@ namespace Systematics.Portal.Web.Controllers
         {
             return View();
         }
-        public IActionResult ContactUs()
-        {
-            /*ContactUsViewModel viewData = new ContactUsViewModel();
 
-            if (ClaimsPrincipal.Current.Identity.IsAuthenticated)
-            {
-                string firstName = ClaimsPrincipal.Current.FindFirst(ClaimTypes.GivenName).Value;
-                string lastName = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Surname).Value;
-                viewData.Name = firstName + " " + lastName;
-                viewData.Email = ClaimsPrincipal.Current.FindFirst(ClaimTypes.Email).Value;
-            }*/
-
-            //return View(viewData);
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult ContactUs([FromBody] ContactUsViewModel model)
-        {
-            string info = string.Empty;
-            string error = string.Empty;
-
-            try
-            {
-                if (model != null)
-                {
-                    string recipient = "nikoos@landcareresearch.co.nz";
-
-                    string message = "<p>" + model.Name;
-                    if (model.Organisation != null && !model.Organisation.Equals(string.Empty))
-                    {
-                        message += ", " + model.Organisation;
-                        if (model.Location != null && !model.Location.Equals(string.Empty))
-                        {
-                            message += ", " + model.Location;
-                        }
-                    }
-                    else if (model.Location != null && !model.Location.Equals(string.Empty))
-                    {
-                        message += " from " + model.Location;
-                    }
-                    message += " has sent the following comment via the Systematics Collections Data website."
-                        + " To respond to " + model.Name + ", please use the following email address: " + model.Email + ".</p>"
-                        + model.Message;
-
-                    Utility.SendEmail(recipient, model.Subject, message, model.Email);
-
-                    info = "Thank you for your feedback. Your comment will be attended to within the next few days.";
-                }
-                else
-                {
-                    throw new Exception("null model");
-                }
-            }
-            catch (Exception e)
-            {
-                error = "Sorry, an error occurred while processing your request. Error: " + e.Message;
-            }
-
-            JSONBaseModel viewData = new JSONBaseModel()
-            {
-                Error = error,
-                Information = info
-            };
-            return Json(viewData);
-        }
     }
 }
