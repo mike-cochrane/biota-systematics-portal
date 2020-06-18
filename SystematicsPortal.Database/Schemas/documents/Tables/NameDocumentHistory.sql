b94ba015ec183d19e75995aa8e5c7e81640bac00
Martin Herran <herranm@landcareresearch.co.nz>
2020-06-18 23:00:02 +1200

    SYSPTL-231  Adding tables for the different document types and history tables

---
+++ SystematicsPortal.Database/Schemas/documents/Tables/NameDocumentHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [documents].[NameDocumentHistory] (
+    [NameId]				UNIQUEIDENTIFIER	NOT NULL,
+    [Version]				INT					NOT NULL,
+	[SerializedDocument]	XML					NOT NULL,
+    [ValidFrom]				DATETIME2 (2)		NOT NULL,
+    [ValidTo]				DATETIME2 (2)		NOT NULL
+);
+
+GO
+CREATE CLUSTERED INDEX [idxNameDocumentHistory]
+    ON [documents].[NameDocumentHistory]([ValidTo] ASC, [ValidFrom] ASC);
---
+++ SystematicsPortal.Database/Schemas/documents/Tables/VernacularNameDocumentHistory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [documents].[VernacularNameDocumentHistory] (
+    [VernacularNameId]				UNIQUEIDENTIFIER	NOT NULL,
+    [Version]				INT					NOT NULL,
+	[SerializedDocument]	XML					NOT NULL,
+    [ValidFrom]				DATETIME2 (2)		NOT NULL,
+    [ValidTo]				DATETIME2 (2)		NOT NULL
+);
+
+GO
+CREATE CLUSTERED INDEX [idxVernacularNameDocumentHistory]
+    ON [documents].[VernacularNameDocumentHistory]([ValidTo] ASC, [ValidFrom] ASC);
