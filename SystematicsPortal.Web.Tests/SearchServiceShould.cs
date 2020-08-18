099cf28e2155d99546832d208c7197d2eb200c2c
Martin Herran <herranm@landcareresearch.co.nz>
2020-08-18 13:36:42 +1200

    SYSPTL-154 Adding easy way to use search partial from UI

--- SystematicsPortal.Web.Tests/SearchServiceShould.cs
+++ SystematicsPortal.Web.Tests/SearchServiceShould.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -20,7 +21,8 @@ namespace SystematicsPortal.Web.Tests
 
             var searchResult = new SearchResult();
 
-            dataClientMock.Setup(foo => foo.Search(GetQuery())).Returns(GetSearchResult());
+            var query = GetQuery();
+            dataClientMock.Setup(foo => foo.Search(query)).Returns(GetSearchResult());
 
             _sut = new SearchService(dataClientMock.Object);
         }
@@ -77,27 +79,103 @@ namespace SystematicsPortal.Web.Tests
         public async Task DoSearchWithFacetsAsync()
         {
             // Arrange
-            var searchTerm = "Bacteria";
-            var selectedPage = 1;
-            var numberResults = 100;
+            string searchTerm = "Bacteria";
+            int selectedPage = 1;
+            int numberResults = 100;
+           string sortField = string.Empty;
+            string sortOrder = "ascending";
+
+            string selectedFacet = "order_ss";
+            string selectedFacetType = "text";
+            string selectedValue = "Coleoptera";
+            string appliedFacetsString = string.Empty;
+
+            // Not sure how the UI uses this toggle
+            bool toggleOn = true;
 
 
-            var sortField = string.Empty;
-            var sortOrder = "ascending";
-            var query = GetQuery(searchTerm, selectedPage, numberResults);
+            var appliedFacets = _sut.SetAppliedFacets(appliedFacetsString, selectedFacet, selectedValue, selectedFacetType, toggleOn);
 
+            var jsonAppliedFacets = JsonConvert.SerializeObject(appliedFacets);
 
             // Act
-            var result = await _sut.Search(query.TextQuery, null, null, selectedPage, query.Rows, sortField, sortOrder);
+            var result = await _sut.Search(searchTerm, appliedFacets, null, selectedPage, numberResults, sortField, sortOrder);
+
 
             // Assert
-
-            // TODO: Check why mocked client is returning null
-            //Assert.IsType<SearchResult>(result);
-            //Assert.NotNull(result);
-
             Assert.True(true);
         }
 
+
+        [Fact]
+        public async Task DoSearchWithFacetsWhenAlreadyFacetAppliedAsync()
+        {
+            // Arrange
+            string searchTerm = "Animalia";
+            int selectedPage = 1;
+            int numberResults = 100;
+            string sortField = string.Empty;
+            string sortOrder = "ascending";
+
+            string selectedFacet = "family_ss";
+            string selectedFacetType = "text";
+            string selectedValue = "Hydrophilidae";
+            string appliedFacetsString = "|order_ss|Coleoptera";
+
+            // Not sure how the UI uses this toggle
+            bool toggleOn = true;
+
+
+            var appliedFacets = _sut.SetAppliedFacets(appliedFacetsString, selectedFacet, selectedValue, selectedFacetType, toggleOn);
+
+            var jsonAppliedFacets = JsonConvert.SerializeObject(appliedFacets);
+
+            // Act
+            var result = await _sut.Search(searchTerm, appliedFacets, null, selectedPage, numberResults, sortField, sortOrder);
+
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task DoSearchWithRangesWhenAlreadyRangeAppliedAsync()
+        {
+            // TODO: We still don't have a range filter. Come back to this test when one is created.
+            // Arrange
+            string searchTerm = "Bacteria";
+            int selectedPage = 1;
+            int numberResults = 100;
+            string sortField = string.Empty;
+            string sortOrder = "ascending";
+
+            string selectedFacet = "order_ss";
+            string selectedFacetType = "range";
+            string selectedValue = "20";
+            string selectedUpperValue = "200";
+            string appliedRangesString = "|added_ss|19360102|19830329";
+
+
+            // Not sure how the UI uses this toggle
+            bool toggleOn = true;
+
+            var query = SystematicsPortal.Web.Helpers.Utility.ReplaceEscapedCharacters(searchTerm);
+
+            var appliedRanges = _sut.SetAppliedRanges(appliedRangesString, selectedFacet, selectedValue, selectedFacetType, selectedUpperValue, toggleOn);
+
+            var jsonAppliedFacets = JsonConvert.SerializeObject(appliedRanges);
+
+            // Act
+            var result = await _sut.Search(query, null, appliedRanges, selectedPage, numberResults, sortField, sortOrder);
+
+
+            // Assert
+            Assert.True(true);
+        }
+
+        
     }
 }
+
+
+
