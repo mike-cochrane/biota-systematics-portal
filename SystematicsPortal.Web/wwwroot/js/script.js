b62d91a3b3d2a3df839a0532dd15314af01a2910
Ben Ridgen <RidgenB@landcareresearch.co.nz>
2020-08-21 16:37:08 +1200

    SYSPTL-369 Implemented icon to match new company website

--- SystematicsPortal.Web/wwwroot/js/script.js
+++ SystematicsPortal.Web/wwwroot/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-    
+    $("#back-to-top").hide(); //hide your div initially
+    var topOfOthDiv = $("#section-head").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+            $("#back-to-top").fadeIn(250); //reached the desired point -- show div
+        } else {
+            $("#back-to-top").fadeOut(250);
+        }
+    });
 });
 
 function ToggleVisibility(el, collapsibleItem, showItem) {
