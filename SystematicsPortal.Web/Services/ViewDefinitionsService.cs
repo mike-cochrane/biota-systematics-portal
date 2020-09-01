2e50dd7a2af8149ba892c2d2ac0a29033cfcd01b
Ben Ridgen <RidgenB@landcareresearch.co.nz>
2020-09-01 13:12:58 +1200

    SYSPTL-252 altered biostatus

--- SystematicsPortal.Web/Services/ViewDefinitionsService.cs
+++ SystematicsPortal.Web/Services/ViewDefinitionsService.cs
@@ -20,7 +20,7 @@ namespace SystematicsPortal.Web.Services
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Reference", XPath = "Document/ParentReference", Order = 40, Template = "ReferenceHyperlink" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "CurrentName", XPath = "Document/CurrentName", Order = 50, Template = "NameHyperlink" });
 
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Biostatus", XPath = "Document/BiostatusValues", Order = 60, Template = "Biostatus" });
+            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Biostatus Values", XPath = "Document/BiostatusValues", Order = 60, Template = "Biostatus" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Concepts", XPath = "Document/Concepts", Order = 70, Template = "Concepts" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Siblings", XPath = "Document/Siblings", Order = 80, Template = "Siblings" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Collections", XPath = "Document/CollectionObjects", Order = 90, Template = "Collections" });
