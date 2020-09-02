ee227953da61fa5ec975ff1a31361714acb158a1
Ben Ridgen <RidgenB@landcareresearch.co.nz>
2020-09-02 16:56:33 +1200

    SYSPTL-397 removed some unneeded sections

--- SystematicsPortal.Web/Services/ViewDefinitionsService.cs
+++ SystematicsPortal.Web/Services/ViewDefinitionsService.cs
@@ -13,12 +13,12 @@ namespace SystematicsPortal.Web.Services
         {
             var viewDefinition = new ViewDefinition();
 
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Parent", XPath = "Document/Parent", Order = 15, Template = "NameHyperlink" });
+            //viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Parent", XPath = "Document/Parent", Order = 15, Template = "NameHyperlink" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Heading", XPath = "Document", Order = 10, Template = "Heading" });
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Authors", XPath = "Document/Authors", Order = 20, Template = "Label" });
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Classification", XPath = "Document/Classification", Order = 30, Template = "Label" });
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Reference", XPath = "Document/ParentReference", Order = 40, Template = "ReferenceHyperlink" });
-            viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "CurrentName", XPath = "Document/CurrentName", Order = 50, Template = "NameHyperlink" });
+            //viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Authors", XPath = "Document/Authors", Order = 20, Template = "Label" });
+            //viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Classification", XPath = "Document/Classification", Order = 30, Template = "Label" });
+            //viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Reference", XPath = "Document/ParentReference", Order = 40, Template = "ReferenceHyperlink" });
+            //viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "CurrentName", XPath = "Document/CurrentName", Order = 50, Template = "NameHyperlink" });
 
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Biostatus Values", XPath = "Document/BiostatusValues", Order = 60, Template = "Biostatus" });
             viewDefinition.FieldDefinitions.Add(new FieldDefinition() { Label = "Concepts", XPath = "Document/Concepts", Order = 70, Template = "Concepts" });
